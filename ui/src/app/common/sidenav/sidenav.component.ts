4935c45433c8690600401d354b0d758b0c5485a1
mnabil1718 <mnabil1718@gmail.com>
2025-10-09 23:52:11 +0700

    ui: fix sidenav link active match

--- ui/src/app/common/sidenav/sidenav.component.ts
+++ ui/src/app/common/sidenav/sidenav.component.ts
@@ -6,21 +6,21 @@ import { Store } from '@ngrx/store';
 
 import * as ChatSelectors from '../../chat/store/chat.selector';
 import { AsyncPipe } from '@angular/common';
-import { ActivatedRoute, RouterLink, UrlSegment } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'sidenav-component',
-  imports: [MatListModule, MatIconModule, MatDividerModule, AsyncPipe, RouterLink],
+  imports: [
+    MatListModule,
+    MatIconModule,
+    MatDividerModule,
+    AsyncPipe,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './sidenav.template.html',
 })
 export class SidenavComponent {
   private store = inject(Store);
-  private route = inject(ActivatedRoute);
   chats$ = this.store.select(ChatSelectors.selectChats);
-
-  readonly activeChatId: string | null;
-
-  constructor() {
-    this.activeChatId = this.route.snapshot.paramMap.get('id');
-  }
 }
